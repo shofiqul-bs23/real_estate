1e2c97325ee0675bc1e2d17085f15c60bc31a56c
shofiqul-bs23 <shofiqul.islam@brainstation-23.com>
2022-04-03 14:48:45 +0600

    Invoice creation implemented while property is being sold.

--- custom_addons/estate_account/models/estate_property.py
+++ custom_addons/estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, Command
 
 
 class EstateProperty(models.Model):
@@ -9,10 +9,22 @@ class EstateProperty(models.Model):
         # print("Is is the Real World ? Or is this a Fantacy?")
         # This is working
 
-
         move_type = 'out_invoice'
-        # journal_id = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
-        
-        self.env['account.move'].create({'partner_id':self.buyer_id, 'move_type':move_type
-                                        })
+        journal_id = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+
+        self.env['account.move'].create(
+            {'partner_id': self.buyer_id, 'move_type': move_type, 'journal_id': journal_id.id,
+             'invoice_line_ids': [
+                 Command.create({
+                     'name': self.name,
+                     'quantity': 1,
+                     'price_unit': self.selling_price * 6 / 100
+                 }),
+                 Command.create({
+                     'name':'administrative fees',
+                     'quantity': 1,
+                     'price_unit' : 100
+                 })
+             ],
+             })
         return super().set_as_sold()
